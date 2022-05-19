a1a131c61cee4b1eb53234f3841441b147352109
francescoP02 <file.partipilo@gmail.com>
2022-05-19 17:42:15 +0200

    Added bonus and hover on card

--- src/components/AppSong.vue
+++ src/components/AppSong.vue
@@ -1,25 +1,31 @@
 <template>
     <div class="playlist">
-        <div class="ms-container pt-4">
+        <div v-if="success" class="ms-container pt-4">
             <div class="row row-cols-6 gap-4">
                 <AppSongCard class="ms-card" v-for="(item, index) in song" :key="index" :songCard="item" />
             </div>
+        </div>
+        <div v-else class="container">
+            <div class="my-loader text-center"><AppLoading /></div>
         </div>
     </div>
 </template>
 
 <script>
 import AppSongCard from "./AppSongCard.vue";
+import AppLoading from "./AppLoading";
 import axios from "axios";
 
 export default {
     name: 'AppSong',
     components: {
         AppSongCard,
+        AppLoading,
     },
     data() {
         return {
             song: [],
+            success: false,
         };
     },
     created() {
@@ -27,6 +33,7 @@ export default {
         .get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((resp) => {
             this.song = resp.data.response;
+            this.success = resp.data.success;
         });
     },
 }
@@ -43,6 +50,21 @@ export default {
     .ms-container {
         width: 60%;
         margin: 0 auto;
+
+        .ms-card {
+            cursor: pointer;
+            transition: 0.3s;
+            &:hover {
+                opacity: 0.7;
+            }
+        }
+    }
+
+    .my-loader {
+        position: absolute;
+        top: 50%;
+        right: 50%;
+        transform: translate(50%, -50%);
     }
 }
 
